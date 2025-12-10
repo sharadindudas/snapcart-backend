d9ed8a6c4318467807dcbb1c0a2f3c86b5af2f1f
Sharadindu Das <sharadindudas774@gmail.com>
2025-12-11 00:53:46 +0530

    chore(env): Fixed the env sample file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 import "dotenv-flow/config";
 
-export const PORT = process.env.PORT!,
+export const NODE_ENV = process.env.NODE_ENV!,
+    PORT = process.env.PORT!,
     SERVER_URL = process.env.SERVER_URL!,
     DATABASE_URL = process.env.DATABASE_URL!,
     FRONTEND_URL = process.env.FRONTEND_URL!;
